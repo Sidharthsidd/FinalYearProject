b7c8842bb3d05af9c4ec03579243147904c4def7
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-21 13:32:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#3333cc",
+        red: "#e60000",
+        secondary: "#555",
+        primaryBG: "#f2f2f2",
+      },
+      fontFamily: {
+        primary: ["Public Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
