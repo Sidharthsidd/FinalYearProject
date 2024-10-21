029cebd59f25ad184efa18f86ce4ec45a102477a
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-21 17:38:04 +0530

    second commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Banner from '../../components/Banner'
+import Categories from './Categories'
+import SpecialDishes from './SpecialDishes'
+
+
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-gradient-to-r from-white via-white to-indigo-500">
+    <Banner/>
+    <Categories/>
+    <SpecialDishes/>
+    </div>
   )
-}
+};
 
 export default Home
