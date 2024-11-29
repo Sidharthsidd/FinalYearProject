eae29acc89376bb75ff90943d4dc460a864e49c9
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-11-29 21:21:10 +0530

    edited render url

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -47,10 +47,10 @@ const Profile = ({ user }) => {
           <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
             <li>
-              <a href="/update-profile">Profile</a>
+              <Link to="/update-profile">Profile</Link>
             </li>
             <li>
-              <a href="/order">Order</a>
+              <Link to="/order">Order</Link>
             </li>
 
             <li>
@@ -60,7 +60,7 @@ const Profile = ({ user }) => {
               <Link to="/dashboard">Dashboard</Link>
             </li>
             <li>
-              <a onClick={handleLogout}>Logout</a>
+              <Link onClick={handleLogout}>Logout</Link>
             </li>
           </ul>
         </div>
