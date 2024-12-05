f4de387545b587729c1238c093196791df53d8e7
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-12-06 02:53:21 +0530

    added ai completely

--- src/components/AiChat.jsx
+++ src/components/AiChat.jsx
@@ -15,7 +15,7 @@ function Chatbot() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:6001/chat', {
+      const response = await fetch('https://finalyearprojectbackend-2gsq.onrender.com/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ userInput: input }),
